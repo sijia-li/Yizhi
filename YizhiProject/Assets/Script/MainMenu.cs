c5a034fbfd67c949a2f0ce9fbfbffbdf36b81c34
ScarlettLi <scartt@bu.edu>
2020-12-21 23:26:39 +0800

    Scene changes
    add scene change to all buttons

--- YizhiProject/Assets/Script/MainMenu.cs
+++ YizhiProject/Assets/Script/MainMenu.cs
@@ -1,18 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    // called when StartButton pressed
+    public void PlayGame() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    // called when ExitButton pressed
+    public void QuitGame() {
+        Debug.Log("QUIT!");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
+        Application.Quit();
+    }
+
+    // called when CreditButton pressed
+    public void Credit() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
     }
 }
---
+++ YizhiProject/Assets/Script/MainSceneUI.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainSceneUI : MonoBehaviour
+{
+    public void QuitGame()
+    {
+        Debug.Log("QUIT!");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        Application.Quit();
+    }
+}
